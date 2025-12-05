7e79038e75bcc43a04464de3d2e6190b1eb13f1b
Dmitry Kostylev <smileyfvce@yandex.ru>
2025-12-05 23:16:52 +0300

    создал ветку и перешёл

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -3,12 +3,17 @@ import '../../index.css';
 import styles from './app.module.css';
 
 import { AppHeader } from '@components';
+import { useLocation, useNavigate } from 'react-router-dom';
 
-const App = () => (
+const App = () => {
+  const location = useLocation();
+  const navigate = useNavigate();
+  const background = location.state && location.state.background;
+return
   <div className={styles.app}>
     <AppHeader />
     <ConstructorPage />
   </div>
-);
 
+}
 export default App;
